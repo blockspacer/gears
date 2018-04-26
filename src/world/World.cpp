8ffded4bc6d62d9fa441af3c8396c03470700a54
faun-a <vadas.balint@gmail.com>
2018-04-26 09:37:45 +0200

    Added TESTING prepocessror

--- src/world/World.cpp
+++ src/world/World.cpp
@@ -5,6 +5,8 @@
 #include "components/BasicComponents.hpp"
 #include "components/UnitComponents.hpp"
 
+#include <random>
+
 namespace ge {
 
 World::World()
@@ -14,27 +16,35 @@ World::World()
     addSystem(m_renderingSystem);
     addSystem(m_selectionSystem);
 
+#ifdef TESTING
+
     // test dwarf entity
     static auto tex = sf::Texture();
     tex.loadFromFile("res/img/DwarvenKing.png");
     static auto sprt = sf::Sprite(tex);
     sprt.scale(.5f, .5f);
 
-    auto e = createEntity();
-    e.addComponent<cmp::Unit>();
-    e.addComponent<cmp::Body>(16u, 16u);
-    e.addComponent<cmp::Sprite>(sprt);
-    e.addComponent<cmp::Health>(100u, 66u);
-    e.addComponent<cmp::Position>(0.f, 0.f);
-    e.addComponent<cmp::Velocity>(0.f, 0.f);
-    e.addComponent<cmp::Equipment>();
-    e.addComponent<cmp::Inventory>(4u);
-    e.addComponent<cmp::Selectable>();
-    e.activate();
+    for(int i = 0; i < 10000; ++i) {
+
+        auto e = createEntity();
+        e.addComponent<cmp::Unit>();
+        e.addComponent<cmp::Body>(16u, 16u);
+        e.addComponent<cmp::Sprite>(sprt);
+        e.addComponent<cmp::Health>(100u, std::rand() % 100 + 1);
+        e.addComponent<cmp::Position>(std::rand() % (200 * 16) - 100 * 16, std::rand() % (200 * 16) - 100 * 16);
+        e.addComponent<cmp::Velocity>(std::rand() % 4 - 2, std::rand() % 4 - 2);
+        e.addComponent<cmp::Equipment>();
+        e.addComponent<cmp::Inventory>(4u);
+        e.addComponent<cmp::Selectable>();
+        e.activate();
+    }
+
+#endif
 }
 
 World::~World()
 {
+    anax::World::clear();
 }
 
 void World::update(float dt)
